9d9e2d57f602d5e8caf7b51c49dfe86ae9a39275
Joshua Morton <joshanator1995@gmail.com>
2015-03-30 16:25:52 +1100

    added port exposing

--- deploy.js
+++ deploy.js
@@ -15,10 +15,15 @@ try {
 switch (argv._[0]) {
   case 'deploy':
     if (!argv._[1]) break;
+    var port = '';
+    if (config.images && config.images[argv._[1]] && config.images[argv._[1]].port) {
+      port = config.images[argv._[1]].port;
+    }
     request.post(config.server+'/deploy/'+argv._[1], {
       form: {
         baseUrl: config.registry,
-        image: argv._[1]
+        image: argv._[1],
+        port: port
       }
     }).pipe(process.stdout);
     break;
@@ -30,29 +35,38 @@ switch (argv._[0]) {
     request.get(config.server+'/list').pipe(process.stdout);
     break;
   case 'server':
-    console.log('server');
     if (argv._[1]) {
       config.server = argv._[1];
+      fs.writeFileSync(path.resolve(process.env.HOME, '.deploycfg'), JSON.stringify(config));
+      console.log('Server url saved');
     }
-    fs.writeFileSync(path.resolve(process.env.HOME, '.deploycfg'), JSON.stringify(config));
     break;
   case 'docker-registry':
-    console.log('docker-registry');
     if (argv._[1]) {
       config.registry = argv._[1];
+      fs.writeFileSync(path.resolve(process.env.HOME, '.deploycfg'), JSON.stringify(config));
+      console.log('Private docker registry url saved');
     }
-    fs.writeFileSync(path.resolve(process.env.HOME, '.deploycfg'), JSON.stringify(config));
+    break;
+  case 'config':
+    if (argv._[1]) {
+      config.images = config.images || {};
+      config.images[argv._[1]] = config.images[argv._[1]] || {};
+      config.images[argv._[1]].port = argv.p || '';
+      fs.writeFileSync(path.resolve(process.env.HOME, '.deploycfg'), JSON.stringify(config));
+    }
     break;
   default:
     console.log([
       'Usage: node deploy.js COMMAND ARGS',
       '',
       ' Available commands:',
-      '   deploy DOCKER_IMAGE     deploys a docker image to the configured server',
-      '   undeploy DOCKER_IMAGE   undeploys a docker image to the configured server',
-      '   list                    lists all docker images currently deployed',
-      '   server URL              sets the url as the server for deploying',
-      '   docker-registry URL     sets the docker registry base url',
+      '   deploy DOCKER_IMAGE           deploys a docker image to the configured server',
+      '   undeploy DOCKER_IMAGE         undeploys a docker image to the configured server',
+      '   list                          lists all docker images currently deployed',
+      '   server URL                    sets the url as the server for deploying',
+      '   docker-registry URL           sets the docker registry base url',
+      '   config DOCKER_IMAGE -p PORT   exposes PORT when image is deployed',
       ''
     ].join('\n'));
 }
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get('/health', function (req, res) {
 
 app.post('/deploy/:name', function (req, res) {
   var image = req.body.baseUrl + '/' + req.body.image + (req.body.tag ? req.body.tag : '');
+  var portMapping = (req.body.port && req.body.port+':'+req.body.port) || '';
   var pull = spawn('docker', ['pull', image]);
   var error = '';
   var intervalId = setInterval(function () {
@@ -25,6 +26,7 @@ app.post('/deploy/:name', function (req, res) {
   });
   pull.stderr.on('data', function (data) {
     data = data.toString().trim();
+    console.log(data);
     var matches = data.match('msg="(.*?)"');
     if (matches && matches[1]) {
       error += matches[1] + '\n';
@@ -43,17 +45,38 @@ app.post('/deploy/:name', function (req, res) {
     });
     stop.stdout.on('end', function () {
       console.log('FINISHED STOPPING');
-      var run = spawn('docker', ['run', '-d', '--name', req.params.name, image]);
+      error = '';
+      var run = spawn('docker', ['run', '-d', '-p', portMapping, '--name', req.params.name, image]);
       run.stdout.on('data', function (data) {
         console.log(data.toString().trim());
       });
+      run.stderr.on('data', function (data) {
+        data = data.toString().trim();
+        console.log(data);
+        var matches = data.match('msg="(.*?)"');
+        if (matches && matches[1]) {
+          error += matches[1] + '\n';
+        }
+      });
       run.stdout.on('end', function () {
+        if (error) {
+          clearInterval(intervalId);
+          res.write(error.red);
+          return res.end();
+        }
         console.log(image, 'is now running');
         clearInterval(intervalId);
         image = req.body.baseUrl + '/' + req.body.image.bold +
                 ':' + (req.body.tag ? req.body.tag : 'latest');
+        if (req.body.port) {
+          console.log('Port', req.body.port, 'exposed');
+          res.write('\nPort '+req.body.port+' exposed\n');
+        } else {
+          console.log('No ports exposed');
+          res.write('\nNo ports exposed\n');
+        }
         var str = image + ' is now running\n';
-        res.write('\n' + str.green);
+        res.write(str.green);
         res.end();
       });
     });
@@ -64,7 +87,7 @@ app.post('/undeploy/:name', function (req, res) {
   var stop = spawn('docker', ['rm', '-f', req.params.name]);
   stop.stdout.on('end', function () {
     var str = 'Undeployed ' + req.params.name + '\n';
-    res.send(str.bold.grey);
+    res.send(str.bold.yellow);
     res.end();
   });
 });
@@ -78,9 +101,9 @@ app.get('/list', function (req, res) {
     var index = lines[0].indexOf('CREATED');
     lines = lines.splice(1);
     res.send(lines.map(function (i) {
-      var created = i.substr(index, i.indexOf('ago')+3-index).grey;
+      var created = '('+i.substr(index, i.indexOf('ago')+3-index)+')';
       var name = i.trim().match('.* (.*?)$')[1].bold.blue;
-      return name + ' ('+created+')';
+      return name + ' ' + created.yellow;
     }).join('\n')+'\n');
   });
 });
